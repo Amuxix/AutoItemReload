f0970d67785b18a9cde04ebd8a7a1f8c616ba94a
Amuxix <amuxix@gmail.com>
2018-04-19 14:25:06 +0100

    Fixed some bugs and update to 1.12.

--- src/main/java/me/amuxix/AutoItemReload.java
+++ src/main/java/me/amuxix/AutoItemReload.java
@@ -1,15 +1,11 @@
 package me.amuxix;
 
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerBucketEmptyEvent;
-import org.bukkit.event.player.PlayerBucketFillEvent;
-import org.bukkit.event.player.PlayerItemBreakEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -26,46 +22,64 @@ public class AutoItemReload extends JavaPlugin implements Listener {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this); //Registers the events on this file.
     }
-    
-    private void reloadHand(final Player player, final EquipmentSlot hand, final Material material) {
+
+    private void reloadHand(final Player player, final EquipmentSlot hand) {
         final PlayerInventory inventory = player.getInventory();
-        //final ItemStack heldItem = hand == HAND ? inventory.getItemInMainHand() : inventory.getItemInOffHand();
+        final ItemStack heldItem = (hand == HAND ? inventory.getItemInMainHand() : inventory.getItemInOffHand()).clone();
+        //Arrays.asList(inventory.getContents()).stream().map(itemStack -> itemStack.getData())
         new BukkitRunnable() {
             public void run() {
-                if (inventory.contains(material)) {
+                if (inventory.containsAtLeast(heldItem, 1)) {
                     // This is the item in hand after the event that triggered the reload has resolved
                     final ItemStack itemInHand = hand == HAND ? inventory.getItemInMainHand() : inventory.getItemInOffHand();
-                    player.sendMessage(itemInHand.toString());
-                    final int first = inventory.first(material);
-                    if(hand == HAND) {
-                        inventory.setItemInMainHand(inventory.getItem(first));
-                    } else {
-                        inventory.setItemInOffHand(inventory.getItem(first));
+                    ItemStack[] contents = inventory.getContents();
+                    ItemStack replacement = null;
+                    Integer replacementSlot = null;
+                    for (int i = 0; i < contents.length; i++) {
+                        ItemStack stack = contents[i];
+                        if (stack == null || stack.getData() == null) continue;
+                        if (stack.getData().equals(heldItem.getData())) {
+                            replacement = stack;
+                            replacementSlot = i;
+                            break;
+                        }
                     }
-                    inventory.setItem(first, itemInHand);
+                    if (replacement == null) {
+                        return;
+                    }
+                    if(hand == HAND) {
+                        inventory.setItemInMainHand(replacement);
+                    } else {
+                        inventory.setItemInOffHand(replacement);
+                    }
+                    inventory.setItem(replacementSlot, itemInHand);
                 }
             }
         }.runTask(this);
+    }
+
+    private EquipmentSlot getEventHand(PlayerBucketEvent event, Player player) {
+        if (event.getItemStack().getData().equals(player.getInventory().getItemInOffHand().getData())) {
+            return OFF_HAND;
+        } else {
+            return HAND;
+        }
     }
 
     @EventHandler(priority = MONITOR)
     public void onBlockPlaceEvent(BlockPlaceEvent event) {
         Player player = event.getPlayer();
         if (event.getItemInHand().getAmount() <= 1 && player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            reloadHand(player, event.getHand(), event.getBlock().getType());
+            reloadHand(player, event.getHand());
         }
     }
-    
-    
+
+
     @EventHandler(priority = MONITOR)
     public void onPlayerBucketEmptyEvent(PlayerBucketEmptyEvent event) {
         Player player = event.getPlayer();
         if(player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            if (event.getItemStack().getType() == player.getInventory().getItemInMainHand().getType()) {
-                reloadHand(player, HAND, event.getBucket());
-            } else {
-                reloadHand(player, OFF_HAND, event.getBucket());
-            }
+            reloadHand(player, getEventHand(event, player));
         }
     }
 
@@ -73,34 +87,30 @@ public class AutoItemReload extends JavaPlugin implements Listener {
     public void onPlayerBucketFillEvent(PlayerBucketFillEvent event) {
         Player player = event.getPlayer();
         if(player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            if (event.getItemStack().getType() == player.getInventory().getItemInMainHand().getType()) {
-                reloadHand(player, HAND, event.getBucket());
-            } else {
-                reloadHand(player, OFF_HAND, event.getBucket());
-            }
+            reloadHand(player, getEventHand(event, player));
         }
     }
-    
+
     @EventHandler(priority = MONITOR)
     public void onPlayerItemBreakEvent(PlayerItemBreakEvent event) {
         Player player = event.getPlayer();
         if(player.getGameMode() != GameMode.CREATIVE) {
             if (player.getInventory().getItemInMainHand().getType() == AIR) {
-                reloadHand(player, HAND, event.getBrokenItem().getType());
+                reloadHand(player, HAND);
             } else {
-                reloadHand(player, OFF_HAND, event.getBrokenItem().getType());
+                reloadHand(player, OFF_HAND);
             }
         }
     }
-    
+
     @EventHandler
     public void onPlayerItemConsumeEvent(PlayerItemConsumeEvent event) {
         Player player = event.getPlayer();
         if (event.getItem().getAmount() <= 1 && player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            if (player.getInventory().getItemInMainHand().getType() == event.getItem().getType()) {
-                reloadHand(player, HAND, event.getItem().getType());
+            if (player.getInventory().getItemInMainHand().getData().equals(event.getItem().getData())) {
+                reloadHand(player, HAND);
             } else {
-                reloadHand(player, OFF_HAND, event.getItem().getType());
+                reloadHand(player, OFF_HAND);
             }
         }
     }
