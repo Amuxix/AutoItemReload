9f17a01a8304f75285ba4074b78b33ce7751d2a2
Amuxix <amuxix@gmail.com>
2022-01-27 17:40:01 +0000

    Update to 1.18 using papermc

--- src/main/java/me/amuxix/AutoItemReload.java
+++ src/main/java/me/amuxix/AutoItemReload.java
@@ -33,43 +33,28 @@ public class AutoItemReload extends JavaPlugin implements Listener {
                     // This is the item in hand after the event that triggered the reload has resolved
                     final ItemStack itemInHand = hand == HAND ? inventory.getItemInMainHand() : inventory.getItemInOffHand();
                     ItemStack[] contents = inventory.getContents();
-                    ItemStack replacement = null;
-                    Integer replacementSlot = null;
-                    for (int i = 0; i < contents.length; i++) {
-                        ItemStack stack = contents[i];
-                        if (stack == null || stack.getData() == null) continue;
-                        if (stack.getData().equals(heldItem.getData())) {
-                            replacement = stack;
-                            replacementSlot = i;
-                            break;
-                        }
-                    }
+                    int replacementSlot = inventory.first(heldItem);
+                    ItemStack replacement = inventory.getItem(replacementSlot);
+
                     if (replacement == null) {
                         return;
                     }
-                    if(hand == HAND) {
+                    inventory.setItem(hand, replacement);
+                    /*if(hand == HAND) {
                         inventory.setItemInMainHand(replacement);
                     } else {
                         inventory.setItemInOffHand(replacement);
-                    }
+                    }*/
                     inventory.setItem(replacementSlot, itemInHand);
                 }
             }
         }.runTask(this);
     }
 
-    private EquipmentSlot getEventHand(PlayerBucketEvent event, Player player) {
-        if (event.getItemStack().getData().equals(player.getInventory().getItemInOffHand().getData())) {
-            return OFF_HAND;
-        } else {
-            return HAND;
-        }
-    }
-
     @EventHandler(priority = MONITOR)
     public void onBlockPlaceEvent(BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        if (event.getItemInHand().getAmount() <= 1 && player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
+        if (event.getItemInHand().getAmount() <= 1 && player.getGameMode() != GameMode.CREATIVE && !event.isCancelled()) {
             reloadHand(player, event.getHand());
         }
     }
@@ -78,23 +63,23 @@ public class AutoItemReload extends JavaPlugin implements Listener {
     @EventHandler(priority = MONITOR)
     public void onPlayerBucketEmptyEvent(PlayerBucketEmptyEvent event) {
         Player player = event.getPlayer();
-        if(player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            reloadHand(player, getEventHand(event, player));
+        if (player.getGameMode() != GameMode.CREATIVE && !event.isCancelled()) {
+            reloadHand(player, event.getHand());
         }
     }
 
     @EventHandler(priority = MONITOR)
     public void onPlayerBucketFillEvent(PlayerBucketFillEvent event) {
         Player player = event.getPlayer();
-        if(player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            reloadHand(player, getEventHand(event, player));
+        if (player.getGameMode() != GameMode.CREATIVE && !event.isCancelled()) {
+            reloadHand(player, event.getHand());
         }
     }
 
     @EventHandler(priority = MONITOR)
     public void onPlayerItemBreakEvent(PlayerItemBreakEvent event) {
         Player player = event.getPlayer();
-        if(player.getGameMode() != GameMode.CREATIVE) {
+        if (player.getGameMode() != GameMode.CREATIVE) {
             if (player.getInventory().getItemInMainHand().getType() == AIR) {
                 reloadHand(player, HAND);
             } else {
@@ -106,8 +91,8 @@ public class AutoItemReload extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerItemConsumeEvent(PlayerItemConsumeEvent event) {
         Player player = event.getPlayer();
-        if (event.getItem().getAmount() <= 1 && player.getGameMode() != GameMode.CREATIVE && event.isCancelled() == false) {
-            if (player.getInventory().getItemInMainHand().getData().equals(event.getItem().getData())) {
+        if (event.getItem().getAmount() <= 1 && player.getGameMode() != GameMode.CREATIVE && !event.isCancelled()) {
+            if (player.getInventory().getItemInMainHand() == event.getItem()) {
                 reloadHand(player, HAND);
             } else {
                 reloadHand(player, OFF_HAND);
